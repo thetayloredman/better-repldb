7338352ffb2abbe9938ada1770ed9f854df6f4b4
thetayloredman <nutdriver716@gmail.com>
2020-09-22 16:38:15 -0700

    Constructor

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const Database = require('@replit/database')
 
 // Class
 class DB extends Map {
-    
+    /**
+     * Creates a new DB.
+     * @constructor
+     * @param {String} name The name of the "table" (Where all objects are stored)
+     * @example
+     * const DB = require('better-repldb');
+     * 
+     * // New DB
+     * const myDB = new DB("myDB");
+     */
+    constructor (name) {
+        super();
+    }
 }
 
 // Export
