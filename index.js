8ec235aae20edd8dee528f8404430e15bbdf7d9d
thetayloredman <nutdriver716@gmail.com>
2020-09-23 09:00:10 -0700

    ae

--- index.js
+++ index.js
@@ -15,6 +15,32 @@ class DB extends Map {
      */
     constructor (name) {
         super();
+        this.db = new Database();
+        this.name = name;
+    }
+
+    /* BEGIN INTERNAL METHODS */
+
+
+
+    /* END INTERNAL METHODS */
+
+    /**
+     * Gets a value from the Database.
+     * @function
+     * @param {String} key The database key to get.
+     * @returns {*} The value
+     */
+    get (key) {
+        let value = null;
+        this.db.get(this.name + ':' + key).then((data) => {
+            value = data;
+        });
+        return value;
+    }
+
+    set (key, value) {
+        this.db.set(this.name + ':' + key, value)
     }
 }
 
